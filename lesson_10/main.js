e94c6f44ff477f6b5c85adcf7c95930a81b45e86
Vadym <vadim.vsnnschuk@gmail.com>
2022-12-16 09:59:23 +0200

    lesson 10 fetch, recursion

---
+++ lesson_10/main.js
@@ -0,0 +1,28 @@
+// зробити файл users.html
+// з ендпоінту http://jsonplaceholder.typicode.com/users отримати всіх користувачів
+// вивести їх id + name списком та додати посилання з href = user-details.html?id=XXX (замість ХХХ - user id)
+// при кліку на посилання перехід на відповідну сторінку, на якій буде вся інформація про користувача (всі 15 полів)
+// отримана через додатковий запит (https://jsonplaceholder.typicode.com/users/XXX   ХХХ - айді користувача)
+
+let ul = document.createElement("ul");
+document.body.appendChild(ul);
+
+
+fetch('http://jsonplaceholder.typicode.com/users')
+    .then(value => value.json())
+    .then(users => {
+
+        for (const user of users) {
+
+            let a = document.createElement('a');
+            a.href = `user-details.html?id=${user.id}`
+            a.innerText = 'user details'
+
+            let li = document.createElement('li');
+            li.innerText =` ${user.id} ${user.name} `
+            li.appendChild(a);
+
+            ul.appendChild(li)
+        }
+
+    })
---
+++ lesson_10/user-details.js
@@ -0,0 +1,22 @@
+let url = new URL(location.href);
+let id = url.searchParams.get('id');
+
+fetch(`http://jsonplaceholder.typicode.com/users/${id}`)
+    .then(value => value.json())
+    .then(user => {
+        function userItem(user) {
+            for (const userKey in user) {
+                let div = document.createElement("div");
+                div.innerHTML = `<b>${userKey}</b>:`;
+                document.body.appendChild(div);
+
+                if (typeof user[userKey] !== "object") {
+                    div.innerHTML = `<b>${userKey}</b>: ${user[userKey]}`;
+                } else {
+                    userItem(user[userKey]);
+                }
+            }
+        }
+
+        userItem(user);
+    })
